a66244d2db697ae07236d811c3dbf4c5dc530c36
Hypeiria <vitia201020@gmail.com>
2026-01-19 17:45:39 -0500

    Added support for RHEL based Linux

--- Service-SOC/Graylog/0-Scripts/LIN - (Sidecar) WIP.sh
+++ Service-SOC/Graylog/0-Scripts/LIN - (Sidecar) WIP.sh
@@ -17,7 +17,7 @@ if [ "$linux_system" != "1" ] && [ "$linux_system" != "2" ]; then
     exit 1
 fi
 
-read -p "Enter GRAYLOG server API ip (Ex: http://<IP_HERE>:9000/api): " manager_ip
+read -p "Enter GRAYLOG server API ip address (Ex: http://<IP_HERE>:9000/api): " manager_ip
 read -p "Enter the API token from GRAYLOG machine (DO NOT SKIP THIS): " gray_token
 
 config_file="/etc/graylog/sidecar/sidecar.yml"
@@ -27,7 +27,7 @@ config_file="/etc/graylog/sidecar/sidecar.yml"
 #  exit 1
 #fi
 
-# Uninstall original agent below
+
 if [ "$linux_system" == "1" ]; then
     wget https://packages.graylog2.org/repo/packages/graylog-sidecar-repository_1-5_all.deb
     sudo dpkg -i graylog-sidecar-repository_1-5_all.deb
@@ -43,12 +43,22 @@ if [ "$linux_system" == "1" ]; then
 
     sudo systemctl enable graylog-sidecar
     sudo systemctl start graylog-sidecar
+
 elif [ "$linux_system" == "2" ]; then
-    echo "Choice 2 was chosen"
+    # Install the repository package
+    sudo rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-sidecar-repository-1-5.noarch.rpm
+    # Install sidecar
+    sudo yum install -y graylog-sidecar
+    sed -i.bak \
+    -e "s|#server_url:.*|server_url: \"$manager_ip\"|" \
+    -e "s|server_api_token:.*|server_api_token: \"$gray_token\"|" \
+    "$config_file"
+
+    sudo graylog-sidecar -service install
+    sudo systemctl enable graylog-sidecar
+    sudo systemctl start graylog-sidecar
+
 fi
 
-
-
-
 echo -e "\n-----------------------------------------"
-echo -e "\n-----------------------------------------\nAttempting to download and install Sidecar...n"
+echo -e "\n-----------------------------------------\nDownloading and installing Sidecar...\n"
